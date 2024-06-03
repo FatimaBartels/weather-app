af1e1fea56ece1a01ed03a0e3f032f0786aca13e
FatimaBartels <bartels.fatima@gmail.com>
2024-06-03 13:44:56 +0200

    search functionality using API

--- src/app.js
+++ src/app.js
@@ -1,16 +1,29 @@
+function refreshWeather(response) {
+  let currentTemperature = document.querySelector("#temperature");
+  let temperature = response.data.temperature.current;
+  let cityElement = document.querySelector("#city-name");
+
+  cityElement.innerHTML = response.data.city;
+
+  currentTemperature.innerHTML = Math.round(temperature);
+}
+
 function searchCity(city) {
   let apiKey = "f80c43e79144055afb51f7885ft48o7e";
-  let apiUrl = `https://api.shecodes.io/weather/v1/current?query={query}&key={apiKey}&units=metric`;
+  let apiUrl = `https://api.shecodes.io/weather/v1/current?query=${city}&key=${apiKey}&units=metric`;
   // make api call and update the interface
+  axios.get(apiUrl).then(refreshWeather);
 }
 
 function searchSubmit(event) {
   event.preventDefault();
   let searchInput = document.querySelector("#form-input");
-  let cityElement = document.querySelector("#city-name");
-  cityElement.innerHTML = searchInput.value;
+
+  searchCity(searchInput.value);
 }
 
 let searchFormElement = document.querySelector(".search-form");
 
 searchFormElement.addEventListener("submit", searchSubmit);
+
+searchCity("Brussel");
