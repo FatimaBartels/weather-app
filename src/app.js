d717dfff605b83e5a368028566c20e21d199d29b
FatimaBartels <bartels.fatima@gmail.com>
2024-06-03 10:33:43 +0200

    added api

--- src/app.js
+++ src/app.js
@@ -1,3 +1,9 @@
+function searchCity(city) {
+  let apiKey = "f80c43e79144055afb51f7885ft48o7e";
+  let apiUrl = `https://api.shecodes.io/weather/v1/current?query={query}&key={apiKey}&units=metric`;
+  // make api call and update the interface
+}
+
 function searchSubmit(event) {
   event.preventDefault();
   let searchInput = document.querySelector("#form-input");
