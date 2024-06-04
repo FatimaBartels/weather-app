4b0405d4f4acc6edabe326e8b709c90470263ba7
FatimaBartels <bartels.fatima@gmail.com>
2024-06-04 14:54:22 +0200

    Injected more weather data

--- src/app.js
+++ src/app.js
@@ -2,17 +2,74 @@ function displayWeather(response) {
   let currentTemperature = document.querySelector("#temperature");
   let temperature = response.data.temperature.current;
   let cityElement = document.querySelector("#city-name");
+  let descriptionElement = document.querySelector("#description");
+  let humidityElement = document.querySelector("#humidity");
+  let windSpeedElement = document.querySelector("#windspeed");
+  let timeElement = document.querySelector("#current-date");
+
+  let date = new Date(response.data.time * 1000);
 
   cityElement.innerHTML = response.data.city;
+  //dateYear.innerHTML = showDate();
+  timeElement.innerHTML = formatDate(date);
+  descriptionElement.innerHTML = response.data.condition.description;
+  humidityElement.innerHTML = `${response.data.temperature.humidity}%`;
+  windSpeedElement.innerHTML = `${response.data.wind.speed}km/hr`;
 
   currentTemperature.innerHTML = Math.round(temperature);
+}
+
+/**function showDate() {
+  let currentDate = new Date();
+  
+  let month = months[currentDate.getMonth()];
+  return `${month} ${currentDay}, ${fullYear}`;
+}*/
+
+function formatDate(date) {
+  let currentDay = date.getDate();
+  let fullYear = date.getFullYear();
+  let minutes = date.getMinutes();
+  let hours = date.getHours();
+  let months = [
+    "January",
+    "February",
+    "March",
+    "April",
+    "May",
+    "June",
+    "July",
+    "August",
+    "September",
+    "October",
+    "November",
+    "December",
+  ];
+
+  let days = [
+    "Sunday",
+    "Monday",
+    "Tuesday",
+    "Wednesday",
+    "Thursday",
+    "Friday",
+    "Saturday",
+  ];
+
+  let month = months[date.getMonth()];
+  let day = days[date.getDay()];
+
+  if (minutes < 10) {
+    minutes = `0${minutes}`;
+  }
+  return `${month} ${currentDay}, ${fullYear}<br/>${day}, ${hours}:${minutes}`;
 }
 
 function searchCity(city) {
   let apiKey = "f80c43e79144055afb51f7885ft48o7e";
   let apiUrl = `https://api.shecodes.io/weather/v1/current?query=${city}&key=${apiKey}&units=metric`;
-  // make api call and update the interface
-  axios.get(apiUrl).then(displayhWeather);
+
+  axios.get(apiUrl).then(displayWeather);
 }
 
 function searchSubmit(event) {
